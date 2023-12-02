79918dacb3e2c75228c79c68a8c7954fb75a3497
riku929hr <33782267+riku929hr@users.noreply.github.com>
2023-12-02 21:49:19 +0900

    feat: added component with single state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import type { FC } from 'react';
 import { Heading } from '@chakra-ui/react';
 import './App.css';
-import UseReducerForm from 'components/UseReducerForm';
-
+// import UseReducerForm from 'components/UseReducerForm';
+// import ReduxToolKitForm from 'components/ReduxToolKitForm';
+import SingleStateForm from 'components/SingleStateForm';
 const App: FC = () => (
   <>
     <Heading size="lg" as="h1" my={8}>
       {import.meta.env.VITE_APP_TITLE}
     </Heading>
     {/* <UseStateForm /> */}
-    <UseReducerForm />
+    {/* <UseReducerForm /> */}
+    {/* <ReduxToolKitForm /> */}
+    <SingleStateForm />
   </>
 );
 
