de28b5684dc0df6c674d9f2e99720116c8821989
riku929hr <33782267+riku929hr@users.noreply.github.com>
2023-12-07 22:05:02 +0900

    add deploy workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import type { FC } from 'react';
 import { Heading } from '@chakra-ui/react';
 import './App.css';
+
+import ReduxToolKitForm from 'components/ReduxToolKitForm';
+// import SingleStateForm from 'components/SingleStateForm';
 // import UseReducerForm from 'components/UseReducerForm';
-// import ReduxToolKitForm from 'components/ReduxToolKitForm';
-import SingleStateForm from 'components/SingleStateForm';
+// import UseStateForm from 'components/UseStateForm';
+
 const App: FC = () => (
   <>
     <Heading size="lg" as="h1" my={8}>
       {import.meta.env.VITE_APP_TITLE}
     </Heading>
-    {/* <UseStateForm /> */}
-    {/* <UseReducerForm /> */}
-    {/* <ReduxToolKitForm /> */}
-    <SingleStateForm />
+    <ReduxToolKitForm />
   </>
 );
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths()
-  ]
-})
+  plugins: [react(), tsconfigPaths()],
+  base: '/state-vs-reducer/',
+});
